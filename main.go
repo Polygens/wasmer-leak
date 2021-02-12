3f4913eb3a9c5234ca186f9ef00a4f9b4980c0ee
Willem Gillis <willem.gillis@gmail.com>
2021-02-12 18:26:02 +0100

    simplified memory leak

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"io/ioutil"
-	"math"
-	"os"
 
 	"github.com/sirupsen/logrus"
 	"github.com/wasmerio/wasmer-go/wasmer"
@@ -59,48 +56,15 @@ func main() {
 		logrus.Fatalf("transform function not found: %s", err)
 	}
 
-	for i := 0; i < 320000; i++ {
-		data := memory.Data()
-		copy(data, input)
+	data := memory.Data()
+	copy(data, input)
 
+	for {
 		// Calls the exported function with the start and length of the inputJson in memory
 		// The return value is an integer containing the start and length of the transformed json in memory
-		output, err := transform(0, len(input))
+		_, err := transform(0, len(input))
 		if err != nil {
 			logrus.Fatal("failed to transform: %w", err)
 		}
-
-		// Retrieve integer from the return value
-		memoryLocation, ok := output.(int64)
-		if !ok {
-			logrus.Fatalf("invalid output")
-		}
-
-		// Webassembly is limited to return only a single value but we need two integers:
-		// 1. The pointer of the beginning of the string in memory
-		// 2. The Length of the string
-		// To work around this limitation we use cantor pairing which allows combining and extracting two integers inside a single integer.
-		start, length := invertedCantorPairing(memoryLocation)
-
-		// Retrieve the updated memory of the instance
-		data = memory.Data()
-
-		dataCopy := make([]byte, len(data))
-		copy(dataCopy, data)
-
-		logrus.Infof("Input JSON: %s", input)
-		logrus.Infof("Output JSON: %s", dataCopy[start:start+length])
 	}
-
-	// Wait for user input before exiting container
-	cliInput := bufio.NewScanner(os.Stdin)
-	cliInput.Scan()
-}
-
-func invertedCantorPairing(z int64) (int32, int32) {
-	w := int64(math.Floor((math.Sqrt(float64(8*z+1)) - 1) / 2))
-	t := (w*w + w) / 2
-	y := z - t
-	x := w - y
-	return int32(x), int32(y)
 }
